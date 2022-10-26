01bf316e4a625c913d5a9ec3d59d56b6fb50b238
FrostArrow <aimarmoreno@yahoo.es>
2022-10-26 21:44:41 +0200

    Los datos del usuario se traen mediante una función aparte

--- app/src/main/java/com/example/testfirebase/HomeActivity.java
+++ app/src/main/java/com/example/testfirebase/HomeActivity.java
@@ -2,6 +2,7 @@ package com.example.testfirebase;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.ActionMenuItemView;
 
 import android.content.Context;
 import android.content.Intent;
@@ -29,9 +30,9 @@ public class HomeActivity extends AppCompatActivity {
     private SharedPreferences.Editor editor;
     private Button saveButton, popupB;
     private FirebaseFirestore db;
-    private String email, provider, usuario;
+    private String email, provider, usuario, nombre, apellidos, edad;
     private DocumentReference usuariosC;
-
+    private Menu esteMenu;
 
 
     @Override
@@ -41,12 +42,14 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
         setTitle("Playable");
 
-        db = FirebaseFirestore.getInstance();
 
         //Recogemos los datos de la actividad anterior
         Intent i = getIntent();
         email = i.getStringExtra("email");
         provider = i.getStringExtra("provider");
+
+        db = FirebaseFirestore.getInstance();
+        getUser(); //recogemos los datos del usuario
 
         emailT = findViewById(R.id.emailT);
         providerT = findViewById(R.id.proovedor);
@@ -61,7 +64,6 @@ public class HomeActivity extends AppCompatActivity {
          editor.apply();
 
 
-
         //Evento analytics
         FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(this);
         Bundle b = new Bundle();
@@ -73,6 +75,7 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.home_menu, menu);
+        esteMenu = menu;
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -89,33 +92,37 @@ public class HomeActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-    public void abrirPopupUsuario(){
-        Intent popupWindow = new Intent(HomeActivity.this, PopUpUsuario.class);
-        popupWindow.putExtra("email", email);
+    public void getUser(){
         db.collection("users").document(email).get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
                     public void onSuccess(DocumentSnapshot it) {
-                        popupWindow.putExtra("usuario", it.get("usuario").toString());
                         usuario = it.get("usuario").toString();
-                        popupWindow.putExtra("nombre", it.get("nombre").toString());
-                        popupWindow.putExtra("apellidos", it.get("apellidos").toString());
-                        popupWindow.putExtra("edad", it.get("edad").toString());
-                        startActivity(popupWindow);
+                        nombre = it.get("nombre").toString();
+                        apellidos = it.get("apellidos").toString();
+                        edad = it.get("edad").toString();
+
                     }
                 });
     }
 
+
+    public void abrirPopupUsuario(){
+        Intent popupWindow = new Intent(HomeActivity.this, PopUpUsuario.class);
+        popupWindow.putExtra("email", email);
+            popupWindow.putExtra("usuario", usuario);
+            popupWindow.putExtra("nombre", nombre);
+            popupWindow.putExtra("apellidos", apellidos);
+            popupWindow.putExtra("edad", edad);
+            startActivity(popupWindow);
+
+
+    }
+
     public void guardarUsuario(View view){ //Funcion apuntes para guardar usuario y campos
-
-
 
         //Con update podemos editar campos individualmente
         db.collection("users").document(email);
-
-
-
 
     }
 
